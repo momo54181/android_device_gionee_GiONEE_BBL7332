57e4b0828ad8f2c450ff0ce2c9ff322518db71f4
momo54181 <momo5418@outlook.com>
2023-01-07 18:43:24 +0800

    0

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -82,3 +82,5 @@ TW_EXTRA_LANGUAGES := true
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_USE_TOOLBOX := true
+TW_BRIGHTNESS_PATH := /sys/devices/platform/leds-mt65xx/leds/lcd-backlight/brightness
+TW_CUSTOM_CPU_TEMP_PATH := /sys/devices/virtual/thermal/thermal_zone1/temp
